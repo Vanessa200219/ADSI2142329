77289b7cff9b62cc6678b711a72403f7830cde90
vamendoza <vamendoza19@misena.edu.co>
2021-09-02 18:20:52 -0500

    POO

---
+++ ProgramacionOrientadasAObjetos/Herencia.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/PooEstilos.css">
+    <title>Herecia</title>
+</head>
+<body>
+
+    <header>
+        <br>
+        <h5>Vanessa Alexandra Mendoza Arias ADSI </h5>
+        <br>
+    </header>
+
+    <section>
+
+        <div>
+            <h1><center>HERENCIA Y REUTILIZACION</center></h1>
+        </div>
+
+        <p>
+            Al igual que los hijos heredamos caracteristicas de nuestros padres, en la POO podemos obtener clases hijas
+            con el mismo contenido de su calse padre. De esta manera se obtiene una reutilizacion de las clases, la cual 
+            es una de las propiedades mas deseadas en la progrmacion ya que conlleva un ahorro de tiempo y lineas de codigo.
+            <br><br>
+            La Reutilizacion a sido abordada en la programacion estructural por medio de funciones. Esto permite crear
+            lineas de codigo que pueden ser invocadas tantas veces como sea necesario. En la POO este concepto es extendido a las
+            clases, de manera que no solo se utilizan clases existentes, sino que tambien se asicionen nuevas variables y metodos
+            a estas sin modificar el contenido original. Esta reutilizacion se logra por medio de la herencia de clases, donde 
+            la clase hija tiene a su disposicion todos los miembros de la clase padre, llamada tambien SUPERCLASE o CLASE 
+            BASE. La clase hija es llamada SUBCLASE o CLASE HEREDADA.
+            <br><br>
+
+            Graficamente, en el diagrama de clases la herencia se representa con una flecha desde la subclase, que indica
+            que la subclase hereda los metodos y atributos de la superclase. 
+            <br><br>
+        </p>
+
+        <h2>EJEMPLO</h2>
+
+        <img src="img/Imagen7.png" alt="">
+
+        <br><br><br>
+
+
+
+        <a class="atras" href=" ClaseyObjeto.PHP">Atras</a>
+
+
+        <a class="next" href="Referencias.php">Siguiente</a>
+        
+        
+        
+        
+    </section>
+    
+</body>
+</html>
---
+++ ProgramacionOrientadasAObjetos/Referencias.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="CSS/PooEstilos.css">
+    <title>Referencias</title>
+</head>
+<body>
+
+
+    <header>
+        <br>
+        <h5>Vanessa Alexandra Mendoza Arias ADSI </h5>
+        <br>
+    </header>
+
+    <section>
+
+        <div>
+            <h1><center>REFERENCIAS</center></h1>
+        </div>
+
+        
+        <a href="https://sena-primo.hosted.exlibrisgroup.com/primo-explore/search?vid=SENA&lang=es_ES"><h2>BIBLOTECA DEL SENA</h2></a>
+        
+        <p>
+            <a href="https://sena-primo.hosted.exlibrisgroup.com/primo-explore/search?vid=SENA&lang=es_ES">Programacion Orientada a Objetos</a>
+            <br><br>
+            <a href="https://elibro-net.bdigital.sena.edu.co/es/ereader/senavirtual/70431?page=219">Logica de Programacion Orientada a Objetos</a>
+        </p>
+
+    
+
+
+        
+
+
+
+        <a class="atras" href=" Herencia.PHP">Atras</a>
+
+
+        <a class="next" href="POO.PHP">Inicio</a>
+        
+        
+        
+        
+    </section>
+    
+</body>
+</html>
