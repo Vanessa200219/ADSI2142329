66a43568f5f0bf4eaa15a290b54e9d655cf13fcc
andres <amlozano055@misena.edu.co>
2021-09-03 09:13:11 -0500

    explicacion Clase y Objeto

---
+++ ClaseyObjeto/Aprendiz.php
@@ -0,0 +1,50 @@
+<?php
+
+//Definir la clase
+
+class Aprendiz{
+    private $documento;
+    private $nombre;
+    private $ficha;
+
+
+    //Metodos Setters indicar el valor de la clase
+
+    function setDocumento($documento)
+    {
+       $this->documento=$documento;
+    }
+
+    function setNombre($nombre)
+    {
+        $this->nombre=$nombre;
+    }
+
+    function setFicha($ficha)
+    {
+        $this->ficha=$ficha;
+    }
+
+
+    //Metodos GETTERS obtener el valor de la clase
+
+
+    function getDocumento()
+    {
+        return $this->documento;
+    }
+
+    function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    function getFicha()
+    {
+        return $this->ficha;
+    }
+}
+
+
+
+?>
