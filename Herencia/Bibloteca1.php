2195a402bbb514d4b4bb8782a0966fb38402b0b6
vamendoza <vamendoza19@misena.edu.co>
2021-09-06 18:03:01 -0500

    clases y definiciones

--- Herencia/Bibloteca1.php
+++ Herencia/Bibloteca1.php
@@ -14,13 +14,42 @@
 
 class Bibloteca
 {
+    //es yna coleccion de materiales
+    protected $coleccion; //llenar libros y revistas
+
+    function __construct(){
+        $this->coleccion=array();
+    }
+
+
+    function Adicionar($material)
+    {
+        array_push($this->coleccion,$material);
+    }
+
+
+    function verMaterial()
+    {
+        for ($i=0; $i <count($this->coleccion); $i++) { 
+
+            if ($this->coleccion[$i]=="Revista") {
+                echo $this->coleccion[$i]->getter("tipoMaterial");
+            }else
+                echo $this->coleccion[$i]->getter("tipoMaterial");
+        }
+    }
+
     
-
+    
 }
-
 
 class Material extends Bibloteca {
 
+    protected $tipoMaterial;
+    protected $codigo;
+    protected $autor;
+    protected $titulo;
+    protected $año;
 
     function __construct($tipoMaterial,$codigo,$autor,$titulo,$año)
     {
@@ -97,43 +126,19 @@ class Material extends Bibloteca {
     
 
 
-    /*function mensaje()
-    {
-       echo "Tipo de Material: {$this->tipoMaterial}.
-       <br><br> Codigo: {$this->codigo}.
-       <br><br> Autor: {$this->autor}.
-       <br><br> Titulo: {$this->titulo}.
-       <br><br> año: {$this->año}.";
-    }*/
-
-
     
-}   
+    }   
 
-
-
-
-    /*$obj = new Material("Libro","Aventura",123456,"Luis Gomez","Las Aventuras",2003);
-    echo "<br> Metodos: <br><br>";
-    $obj->AltaMaterial();
-    echo "<br><br>";
-    $obj->BajaMaterial();
-    echo "<br><br>";
-    $obj->CambiarMaterial();
-    echo "<br><br><br> Caracteristicas: <br><br>";
-    $obj->mensaje();*/
     
     
     
     class Libro extends Material{
 
+        protected $editorial;
+
         function __construct($tipoMaterial,$codigo,$autor,$titulo,$año,$editorial)
         {
-            $this->tipoMaterial=$tipoMaterial;
-            $this->codigo=$codigo;
-            $this->autor=$autor;
-            $this->titulo=$titulo;
-            $this->año=$año;
+            parent::__construct($tipoMaterial,$codigo,$autor,$titulo,$año);
             $this->editorial=$editorial;
 
         }
@@ -147,77 +152,45 @@ class Material extends Bibloteca {
             return $this->$nombreAtributo;
         }
 
-        /*function setTipoMaterial($tipoMaterial){
-            $this->tipoMaterial = $tipoMaterial;
-        }
-    
-        function getTipoMaterial(){
-            return $this->tipoMaterial;
-        }
-    
-    
-        function setCodigo($codigo){
-            $this->codigo = $codigo;
-        }
-    
-        function getCodigo(){
-            return $this->codigo;
-        }
-    
-    
-        function setAutor($autor){
-            $this->autor = $autor;
-        }
-    
-        function getAutor(){
-            return $this->autor;
-        }
-    
-    
-        function setTitulo($titulo){
-            $this->titulo = $titulo;
-        }
-    
-        function getTitulo(){
-            return $this->titulo;
-        }
-    
-    
-        function setAño($año){
-            $this->año = $año;
-        }
-    
-        function getAño(){
-            return $this->año;
-        }
 
 
-        function setEditorial($editorial){
-            $this->editorial = $editorial;
-        }
-    
-        function getEditorial(){
-            return $this->editorial;
-        }
+        
+    }
 
 
 
-        function mensaje()
+    class Revista extends Material{
+
+        protected $volumen;
+
+        function __construct($tipoMaterial,$codigo,$autor,$titulo,$año,$volumen)
         {
-            echo "Tipo de Material: {$this->tipoMaterial}.
-            <br><br> Codigo: {$this->codigo}.
-            <br><br> Autor: {$this->autor}.
-            <br><br> Titulo: {$this->titulo}.
-            <br><br> año: {$this->año}.
-            <br><br> Editorial: {$this->editorial}";
-        }*/
+            parent::__construct($tipoMaterial,$codigo,$autor,$titulo,$año);
+            $this->volumen=$volumen;
+
+        }
+
+        function setter($atributo,$valor){
+            $this->$atributo=$valor;
+        }
+
+
+        function getter($nombreAtributo){
+            return $this->$nombreAtributo;
+        }
+
 
 
         
     }
     
     
-    /*$obj = new Libro("Libro",123456,"Luis Gomez","Las Aventuras",2003,"anonimo");
+
+    echo '<br><br>';
+    $obj= new Bibloteca ("Libro");
+    $obj->verMaterial("");
+    $obj= new Libro("Libro",143385,"Gabriel Garcia marquez","Cien años de Soledad",1967,"Mondadori Debolsillo");
+    echo $obj->getter("tipoMaterial").'<br><br>';
     echo "<br> Material: <br><br>";
     $obj->AltaMaterial();
     echo "<br><br>";
@@ -225,12 +198,27 @@ class Material extends Bibloteca {
     echo "<br><br>";
     $obj->CambiarMaterial();
     echo "<br><br><br> Caracteristicas: <br><br>";
-    $obj->mensaje();*/
+
+    //uso de getter abreviado
+    echo "Tipo de Material: ";
+    echo $obj->getter("tipoMaterial").'<br><br>';
+    echo "Codigo: ";
+    echo $obj->getter("codigo").'<br><br>';
+    echo "Autor: ";
+    echo $obj->getter("autor").'<br><br>';
+    echo "Titulo : ";
+    echo $obj->getter("titulo").'<br><br>';
+    echo "Año: ";
+    echo $obj->getter("año").'<br><br>';
+    echo "Editorial: ";
+    echo $obj->getter("editorial").'<br><br>';
 
 
-
-
-    $obj = new Libro("Libro",123456,"Luis Gomez","Las Aventuras",2003,"anonimo");
+    //Nuevo Instaciamineto 
+    echo '<br><br>';
+    $obj= new Bibloteca ("Libro");
+    $obj= new Libro("Libro",143385,"Fiódor Dostoyevski","Crimen y castigo",1866,"Alma");
+    echo $obj->getter("tipoMaterial").'<br><br>';
     echo "<br> Material: <br><br>";
     $obj->AltaMaterial();
     echo "<br><br>";
@@ -255,7 +243,38 @@ class Material extends Bibloteca {
 
 
 
-    echo "<br>Modificacion: <br><br> ";
+    //Nuevo Instaciamineto para Revista
+    echo '<br><br>';
+    $obj= new Bibloteca ("Revista");
+    //$tipoMaterial,$autor,$titulo,$año,$volumen
+    $obj= new Revista("Revista","123123","Publicaciones Semana","Revista Semana",1999,"0124-5473");
+    echo $obj->getter("tipoMaterial").'<br><br>';
+    echo "<br> Material: <br><br>";
+    $obj->AltaMaterial();
+    echo "<br><br>";
+    $obj->BajaMaterial();
+    echo "<br><br>";
+    $obj->CambiarMaterial();
+    echo "<br><br><br> Caracteristicas: <br><br>";
+
+    //uso de getter abreviado
+    echo "Tipo de Material: ";
+    echo $obj->getter("tipoMaterial").'<br><br>';
+    echo "Autor: ";
+    echo $obj->getter("autor").'<br><br>';
+    echo "Codigo: ";
+    echo $obj->getter("codigo").'<br><br>';
+    echo "Titulo : ";
+    echo $obj->getter("titulo").'<br><br>';
+    echo "Año: ";
+    echo $obj->getter("año").'<br><br>';
+    echo "Volumen: ";
+    echo $obj->getter("volumen").'<br><br>';
+
+
+
+    /*echo "<br>Modificacion: <br><br> ";
+
     //uso de setters abreviados
     //Definir Modificaciones
     $obj->setter("tipoMaterial","Revista");
@@ -277,9 +296,13 @@ class Material extends Bibloteca {
     echo "Año: ";
     echo $obj->getter("año").'<br><br>';
     echo "Editorial: ";
-    echo $obj->getter("editorial").'<br><br>';
+    echo $obj->getter("editorial").'<br><br>';*/
 
-    ?>
+   
+
+    
+
+ ?>
     </section>
 </body>
 </html>
