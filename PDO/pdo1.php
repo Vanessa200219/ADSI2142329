78b544c30f64887d829676ef0b213c09b993b45a
vamedoza <vamedoza19@misena.edu.co>
2021-09-10 09:10:08 -0500

    Conexio a base de datos y excepciones

---
+++ PDO/pdo1.php
@@ -0,0 +1,27 @@
+<?php
+
+//PDO = PHP Data Obbject
+
+$dsn='mysql:host=localhost;dbname=adsi';
+$username='root';
+$password='';
+$conexion;
+
+try {
+    $conexion=new PDO($dsn,$username,$password);
+    $statement=$conexion->prepare('SELECT * FROM aprendiz');
+    $statement->execute();
+} catch (PDOException $ob) {
+    echo $ob->getMessage();
+}
+
+var_dump($statement);
+echo '<br><br>';
+foreach ($statement as $key) {
+    echo 'Nombre: '.$key['Nombre'].'-';
+    echo 'Documento: '.$key['Documento'].'-';
+    echo 'Formacion: '.$key['Formacion'].'-';
+    echo 'Genero: '.$key['Genero'].'<br><br>';
+}
+
+?>
