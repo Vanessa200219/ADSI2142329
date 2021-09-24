9f83f0fe8cf49e177850cc0b3bbe8d051d33c74a
vamendoza <vamendoza19@misena.edu.co>
2021-09-24 17:21:08 -0500

    ESTILO PORTAFOLIO

--- Portafolio/PORTAFOLIOWEB/index.php
+++ Portafolio/PORTAFOLIOWEB/index.php
@@ -4,17 +4,41 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/estiloIndex.css">
+    <link rel="stylesheet" href="CSS/index.css">
     <title>PORTAFOLIO</title>
 </head>
 <body>
-    MI PRESENTACION,
-    HOJA DE VIDA,
-    MIS PROYECTOS,
-    MVC,
-    CONTENEDORES,
-    JAVASCRIPT.
+    <div class="dropdown">
 
+        <div class="contenedor">
+		    <nav class="navegacion">
+			    <ul class="menu">
+                    <li><a href=""><p id="estructura"><strong>Infomacion Personal</strong></p></a></li>
+				    <li><a href=""><p id="estructura"><strong>Proyectos</strong></p></a></li>
+				    <li><a href=""><p id="estructura"><strong>Contactos</strong></p></a></li>
+                </ul>
+		    </nav>
+	    </div>
+
+    </div>
+    
+    <section class="section">
+        
+
+        <div class="text-first">
+            <p id="p">DESARROLLO DE APLICACIONES WEB</p>
+            <h3 id="text-second">Desarrollador de Codigo front-end y back-end, Programador de JavaScript y PHP</h3>
+        </div>
+
+        <div class="section2">
+        </div>
+
+    </section>
+
+    <section>
+        <p></p>
+    </section>
+    
     
 </body>
 </html>
