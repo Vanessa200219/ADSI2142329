5d14925cb79d9a044175cbeeb42c50b54a410153
vamendoza <vamendoza19@misena.edu.co>
2021-09-28 19:36:57 -0500

    PORTADA DE PORTAFOLIO

--- Portafolio/PORTAFOLIOWEB/index.php
+++ Portafolio/PORTAFOLIOWEB/index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/index.css">
+    <link rel="stylesheet" href="CSS/estilos.css">
     <title>PORTAFOLIO</title>
 </head>
 <body>
@@ -15,7 +15,7 @@
 			    <ul class="menu">
                     <li><a href=""><p id="estructura"><strong>Infomacion Personal</strong></p></a></li>
 				    <li><a href=""><p id="estructura"><strong>Proyectos</strong></p></a></li>
-				    <li><a href=""><p id="estructura"><strong>Contactos</strong></p></a></li>
+				    <li><a href=""><p id="estructura"><strong>Manuales</strong></p></a></li>
                 </ul>
 		    </nav>
 	    </div>
@@ -27,7 +27,9 @@
 
         <div class="text-first">
             <p id="p">DESARROLLO DE APLICACIONES WEB</p>
-            <h3 id="text-second">Desarrollador de Codigo front-end y back-end, Programador de JavaScript y PHP</h3>
+            <h3 id="text-second">Desarrollador de Codigo front-end y back-end, 
+                Programador de JavaScript y PHP
+            </h3>
         </div>
 
         <div class="section2">
@@ -35,9 +37,87 @@
 
     </section>
 
-    <section>
-        <p></p>
+    <section class="Biografia">
+        <p class="p">TECNOLOGO EN SISTEMAS DE INFORMACION</p>
+
+        <p class="texto">Analista de Sistema de Informacion, Programadora de Software y Mantenimiento
+            en Programas.
+        </p>
+        
+        <a class="hojadevida" href="hojadevida.php">HOJA DE VIDA</a>
+        
     </section>
+
+    <br>
+    <section class="proyectos">
+        <p class="titulo">PROYECTOS</p>
+
+        <br>
+        <br>
+        <br>
+        <br>
+
+        <div class="MCV">
+            <section class="color">MVC</section>
+            <a class="vinculo" href="">Ver Sitio</a>
+        </div>
+
+        <div class="contenedores">
+            <section class="color2">CONTENEDORES</section>
+            <a class="vinculo2" href="">Ver Sitio</a>
+        </div>
+        
+        <div class="javascript">
+            <section class="color3">JAVASCRIPT</section>
+            <a class="vinculo" href="">Ver Sitio</a>
+        </div>
+    </section>
+
+
+
+
+    <section class="manual">
+
+
+        <div class="usuario">
+            <section class="color">Manual de Usuarios</section>
+            <a class="vinculo" href="">Ver Sitio</a>
+        </div>
+
+        <div class="software">
+            <section class="one">Manuales</section>
+        </div>
+        
+
+        <div class="software">
+            <section class="color2">Manual de Software</section>
+            <a class="vinculo2" href="">Ver Sitio</a>
+        </div>
+    </section>
+
+            
+            
+
+        
+    </section>
+
+
+    <footer>
+        <br>
+        <section class="bloque1">
+        <p class="num">3202883867</p>
+        <p class="conta1"></p>
+        <p class="conta">Contacto</p>
+        </section>
+
+        <section class="bloque2">
+        <p class="email">Vamendoza19@misena.edu.co</p>
+        <p class="conta2"></p>
+        <p class="corre">Email</p>
+        </section>
+        <br>
+        <p class="año">2021</p>
+    </footer>
     
     
 </body>
