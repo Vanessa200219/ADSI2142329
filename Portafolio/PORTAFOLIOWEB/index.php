26158696818711981821b426a27ab5292c2fe980
vamendoza19 <vamedoza19@misena.edu.co>
2021-10-08 09:11:31 -0500

    CSS

--- Portafolio/PORTAFOLIOWEB/index.php
+++ Portafolio/PORTAFOLIOWEB/index.php
@@ -1,128 +1,132 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/estilo.css">
-    <script src="../estilo.js"></script>
-    <title>PORTAFOLIO</title>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="CSS/index.css">
+<script src="../estilo.js"></script>
+<title>PORTAFOLIO</title>
 </head>
 <body>
-    <div class="dropdown">
+<div class="dropdown">
 
-        <div class="contenedor">
-		    <nav class="navegacion">
-			    <ul class="menu">
-                    <li><a href="#Infor_Persol"><p id="estructura"><strong>Infomacion Personal</strong></p></a></li>
-				    <li><a href="#proyectos"><p id="estructura"><strong>Proyectos</strong></p></a></li>
-				    <li><a href="#manuales"><p id="estructura"><strong>Manuales</strong></p></a></li>
-                </ul>
-		    </nav>
-	    </div>
+<div class="contenedor">
+<nav class="navegacion">
+<ul class="menu">
+<li><a href="#Infor_Persol"><p id="estructura"><strong>Infomacion Personal</strong></p></a></li>
+<li><a href="#proyectos"><p id="estructura"><strong>Proyectos</strong></p></a></li>
+<li><a href="#manuales"><p id="estructura"><strong>Manuales</strong></p></a></li>
+</ul>
+</nav>
+</div>
 
-    </div>
-    
-    <section class="section">
-        
+</div>
 
-        <div class="text-first">
-            <p id="p">DESARROLLO DE APLICACIONES WEB</p>
-            <h3 id="text-second">Desarrollador de Codigo front-end y back-end, 
-                Programador de JavaScript y PHP
-            </h3>
-        </div>
-
-        <div class="section2">
-        </div>
-
-    </section>
-
-    <a id="Infor_Persol" href=""><section class="Biografia">
-        <p class="p">TECNOLOGO EN SISTEMAS DE INFORMACION</p>
-
-        <p class="texto">Analista de Sistema de Informacion, Programadora de Software y Mantenimiento
-            en Programas.
-        </p>
-        
-        <!--Hipervinculos para dentro de la misma pagina-->
-        <a class="hojadevida" href="hojadevida.php">HOJA DE VIDA</a>
+<section class="section">
 
 
-        <a id="proyectos"></a>
+<div class="text-first">
+<p id="p">DESARROLLO DE APLICACIONES WEB</p>
+<h3 id="text-second">Desarrollador de Codigo front-end y back-end, 
+Programador de JavaScript y PHP
+</h3>
+</div>
 
-    </section>
+<div class="section2">
+</div>
 
-        <section class="proyectos">
-        <p class="titulo">PROYECTOS</p>
+</section>
 
-        <br>
-        <br>
-        <br>
-        <br>
+<a id="Infor_Persol" href=""><section class="Biografia">
+<p class="p">TECNOLOGO EN SISTEMAS DE INFORMACION</p>
 
-        <div class="MCV">
-            <section class="color">MVC</section>
+<p class="texto">Analista de Sistema de Informacion, Programadora de Software y Mantenimiento
+en Programas.
+</p>
+
+<!--Hipervinculos para dentro de la misma pagina-->
+<a class="hojadevida" href="hojadevida.php">HOJA DE VIDA</a>
+
+
+<a id="proyectos"></a>
+
+</section>
+<!-- section mvc -->
+<section class="proyectos">
+<p class="titulo">PROYECTOS</p>
+
+<br>
+<br>
+<br>
+<br>
+<section class="container">
+    <div class="container-model">
+        <div class="container-MCV">
+            <div class="titulo-mvc">MVC</div>
             <a class="vinculo" href="../MVCDAO/Vista/">Ver Sitio</a>
         </div>
 
-        <div class="contenedores">
-            <section class="color2">JAVASCRIPT HERENCIA</section>
+        <div class="container-MVC">
+            <div class="titulo-mvc">JAVASCRIPT HERENCIA</div>
             <a class="vinculo2" href="JavaScript/HerenciaProyecto/herencia.php">Ver Sitio</a>
         </div>
-        
-        <div class="javascript">
-            <section class="color3">JAVASCRIPT</section>
+
+        <div class="container-MVC">
+            <div class="titulo-mvc">JAVASCRIPT</div>
             <a class="vinculo" href="JavaScript/java.php">Ver Sitio</a>
         </div>
-    </section>
+    </div>
+</section>
+
+
+</section>
+
+
+
+<a id="manuales" href=""><section class="manual">
+
+
+<div class="usuario">
+<section class="color">Manual de Usuarios</section>
+<a class="vinculo" href="Manual/manualdeusuarios.php">Ver Sitio</a>
+</div>
+
+<div class="software">
+<section class="one">Manuales</section>
+</div>
+
+
+<div class="software">
+<section class="color2">Manual de Software</section>
+<a class="vinculo2" href="Manual/manualdesoftware.php">Ver Sitio</a>
+</div>
+</section></a>
 
 
 
 
-    <a id="manuales" href=""><section class="manual">
+
+</section>
 
 
-        <div class="usuario">
-            <section class="color">Manual de Usuarios</section>
-            <a class="vinculo" href="Manual/manualdeusuarios.php">Ver Sitio</a>
-        </div>
+<footer>
+<br>
+<section class="bloque1">
+<p class="num">3202883867</p>
+<p class="conta1"></p>
+<p class="conta">Contacto</p>
+</section>
 
-        <div class="software">
-            <section class="one">Manuales</section>
-        </div>
-        
-
-        <div class="software">
-            <section class="color2">Manual de Software</section>
-            <a class="vinculo2" href="Manual/manualdesoftware.php">Ver Sitio</a>
-        </div>
-    </section></a>
-
-            
-            
-
-        
-    </section>
+<section class="bloque2">
+<p class="email">Vamendoza19@misena.edu.co</p>
+<p class="conta2"></p>
+<p class="corre">Email</p>
+</section>
+<br>
+<p class="año">2021</p>
+</footer>
 
 
-    <footer>
-        <br>
-        <section class="bloque1">
-        <p class="num">3202883867</p>
-        <p class="conta1"></p>
-        <p class="conta">Contacto</p>
-        </section>
-
-        <section class="bloque2">
-        <p class="email">Vamendoza19@misena.edu.co</p>
-        <p class="conta2"></p>
-        <p class="corre">Email</p>
-        </section>
-        <br>
-        <p class="año">2021</p>
-    </footer>
-    
-    
 </body>
 </html>
