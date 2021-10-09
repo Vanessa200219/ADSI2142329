1a0c9b3b075f7bfeba4dd68a6d4ae46aaaf1994e
vamendoza <vamendoza19@misena.edu.co>
2021-10-08 21:00:37 -0500

    CSS

--- Portafolio/PORTAFOLIOWEB/index.php
+++ Portafolio/PORTAFOLIOWEB/index.php
@@ -1,128 +1,232 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/estilo.css">
-    <script src="../estilo.js"></script>
-    <title>PORTAFOLIO</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="css/estilo.css">
+  <title>PRTAFOLIO</title>
 </head>
+
 <body>
-    <div class="dropdown">
+  <!-- Menu -->
+    <section id="header">
+        <div class="header container">
+            <div class="nav-bar">
+                <nav class="nav-list">
+                    <div class="hamburger">
+                        <div class="bar"></div>
+                    </div>
+                        <ul>
+                            <li><a href="#hero" data-after="Home"><strong>Informacion Personal</strong></a></li>
+                            <li><a href="#services" data-after="Service"><strong>Proyectos</strong></a></li>
+                            <li><a href="#projects" data-after="Projects"><strong>Manuales</strong></a></li>
+                            <li><a href="#contact" data-after="Contact"><strong>Contactos</strong></a></li>
+                        </ul>
+                </nav>
+            </div>
+        </div>
+    </section>
+  <!-- FIN MENU -->
 
-        <div class="contenedor">
-		    <nav class="navegacion">
-			    <ul class="menu">
-                    <li><a href="#Infor_Persol"><p id="estructura"><strong>Infomacion Personal</strong></p></a></li>
-				    <li><a href="#proyectos"><p id="estructura"><strong>Proyectos</strong></p></a></li>
-				    <li><a href="#manuales"><p id="estructura"><strong>Manuales</strong></p></a></li>
-                </ul>
-		    </nav>
-	    </div>
 
+  <!-- PORTADA  -->
+  <section id="hero">
+    <div class="hero container">
+      <div>
+        <h1>DESARROLLO DE APLICACIONES WEB</h1>
+        <h3>Desarrollador de Codigo front-end y back-end, Programador de JavaScript y PHP.<span></span></h3>
+      </div>
     </div>
-    
-    <section class="section">
+  </section>
+  <!-- FIN PORTADA -->
+
+
+
+<!-- Biografia -->
+<section class="Biografia">
+    <h1 class="subtitle">TECNOLOGO EN SISTEMAS DE INFORMACION</h1>
+        <p class="presentation__copy">Analista de Sistema de Informacion, Programadora de Software y Mantenimiento en Programas.</p>
+        <a href="hojadevida.php" type="button" class="cta">HOJA DE VIDA</a>
+</section>
+<!-- FIN  BIOGRAFIA -->
+
+
+
+
+
+
+  <!-- Service Section -->
+  <section id="services">
+    <div class="services container">
+      <div class="service-top">
+        <h1 class="section-title">Proyectos</h1>
+
+      
+      <div class="service-bottom">
+
+        <div class="service-item">
+          <h2>MVC</h2>
+          <a class="vinculo" href="../MVCDAO/Vista/">Ver Sitio</a>
+        </div>
+
+
         
 
-        <div class="text-first">
-            <p id="p">DESARROLLO DE APLICACIONES WEB</p>
-            <h3 id="text-second">Desarrollador de Codigo front-end y back-end, 
-                Programador de JavaScript y PHP
-            </h3>
+        
+        <div class="service-item">
+          <h2>JavaScript</h2>
+          <a class="vinculo" href="../MVCDAO/Vista/">Ver Sitio</a>
+
         </div>
 
-        <div class="section2">
+
+        <div class="service-item">
+          <h2>JavaScript Herencia</h2>
+          <a class="vinculo" href="../MVCDAO/Vista/">Ver Sitio</a>
+
         </div>
 
-    </section>
 
-    <a id="Infor_Persol" href=""><section class="Biografia">
-        <p class="p">TECNOLOGO EN SISTEMAS DE INFORMACION</p>
+      </div> 
+    </div>
+  </section>
+  <!-- End Service Section -->
 
-        <p class="texto">Analista de Sistema de Informacion, Programadora de Software y Mantenimiento
-            en Programas.
+  <!-- Projects Section -->
+  <section id="projects">
+    <div class="projects container">
+      <div class="projects-header">
+        <h1 class="section-title">Proyectos<span>Recientes</span></h1>
+      </div>
+      <div class="all-projects">
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Proyecto 1</h1>
+            <h2>Dao CRUD Aplicado a tabala de Proyecto</h2>
+            <p>Creacion de ejemplo de PDO con metodo dao en el cual aplicamos dicha metodologia a una tabla del proyecto de formacion SENA</p>
+          </div>
+          <div class="project-img">
+            <img src="./imagenes/images5.jpg" alt="imagenes">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Proyecto 2</h1>
+            <h2>Operaciones Aritmeticas con Arrays JS</h2>
+            <p>Ejercicio en el cual generamos un Array que fuera actualizando sus valores al recargar la pagina y creamos funciones de suma de los elementos del 
+            Array y tambien funciones que diera como respuesta en consola el numero mayor ,el numero menor , y el promedio 
+            de los valores del Array </p>
+          </div>
+          <div class="project-img">
+            <img src="./imagenes/images6.png" alt="imagenes">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>Proyecto 3</h1>
+            <h2>Fracciones JSON</h2>
+            <p>Ejercicio de suma de fraccinarion en javaScript</p>
+          </div>
+          <div class="project-img">
+            <img src="./imagenes/images7.jpg" alt="imagenes">
+          </div>
+        </div>
+        <div class="project-item">
+          <div class="project-info">
+            <h1>cargue aqui proximo proyecto</h1>
+            <h2>futuro proyecto</h2>
+            <p></p>
+          </div>
+          <div class="project-img">
+            <img src="./img/img-1.png" alt="img">
+          </div>
+        </div>
+      </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Projects Section -->
+
+  <!-- About Section -->
+  <section id="about">
+    <div class="about container">
+      <div class="col-left">
+        <div class="about-img">
+          <img src="./imagenes/images4.jpeg" alt="imagenes">
+        </div>
+      </div>
+      <div class="col-right">
+        <h1 class="section-title">Sobre<span>Mi</span></h1>
+        <h2>Desarrollador Wed</h2>
+        <p>sdfdfsdddsffffffffffffffffffffffffffffffdsfsdfsdddddddddddddddddddddddddfsdfsdfsdfsdfsdfssss
+            sdfsdfsdfsdfsfssfsd
         </p>
-        
-        <!--Hipervinculos para dentro de la misma pagina-->
-        <a class="hojadevida" href="hojadevida.php">HOJA DE VIDA</a>
+        <a href="#" class="cta">Descargue HV</a>
+      </div>
+    </div>
+  </section>
+  <!-- End About Section -->
 
-
-        <a id="proyectos"></a>
-
-    </section>
-
-        <section class="proyectos">
-        <p class="titulo">PROYECTOS</p>
-
-        <br>
-        <br>
-        <br>
-        <br>
-
-        <div class="MCV">
-            <section class="color">MVC</section>
-            <a class="vinculo" href="https://localhost/ADSI2142329/Portafolio/MVCDAO/Vista/">Ver Sitio</a>
+  <!-- Contact Section -->
+  <section id="contact">
+    <div class="contact container">
+      <div>
+        <h1 class="section-title">Informacion<span>Contactos</span></h1>
+      </div>
+      <div class="contact-items">
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/phone.png" /></div>
+          <div class="contact-info">
+            <h1>Telefono</h1>
+            <h2>+57 3014391260</h2>
+            <h2>+1 234 123 1234</h2>
+          </div>
         </div>
-
-        <div class="contenedores">
-            <section class="color2">JAVASCRIPT HERENCIA</section>
-            <a class="vinculo2" href="JavaScript/HerenciaProyecto/herencia.php">Ver Sitio</a>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/new-post.png" /></div>
+          <div class="contact-info">
+            <h1>Email</h1>
+            <h2>bccuervo0@misena.edu.co</h2>
+            <h2>darkcrow-1@hotmail.com</h2>
+          </div>
         </div>
-        
-        <div class="javascript">
-            <section class="color3">JAVASCRIPT</section>
-            <a class="vinculo" href="JavaScript/java.php">Ver Sitio</a>
+        <div class="contact-item">
+          <div class="icon"><img src="https://img.icons8.com/bubbles/100/000000/map-marker.png" /></div>
+          <div class="contact-info">
+            <h1>Ubicacion</h1>
+            <h2>Cundinamarca, Soacha</h2>
+          </div>
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
+  <!-- End Contact Section -->
 
-
-
-
-    <a id="manuales" href=""><section class="manual">
-
-
-        <div class="usuario">
-            <section class="color">Manual de Usuarios</section>
-            <a class="vinculo" href="Manual/manualdeusuarios.php">Ver Sitio</a>
+  <!-- Footer -->
+  <section id="footer">
+    <div class="footer container">
+      <div class="brand">
+        <h1><span>C</span>amilo <span>C</span>uervo</h1>
+      </div>
+      <h2>Solucion Wed</h2>
+      <div class="social-icon">
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/facebook-new.png" /></a>
         </div>
-
-        <div class="software">
-            <section class="one">Manuales</section>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/instagram-new.png" /></a>
         </div>
-        
-
-        <div class="software">
-            <section class="color2">Manual de Software</section>
-            <a class="vinculo2" href="Manual/manualdesoftware.php">Ver Sitio</a>
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/twitter.png" /></a>
         </div>
-    </section></a>
-
-            
-            
-
-        
-    </section>
-
-
-    <footer>
-        <br>
-        <section class="bloque1">
-        <p class="num">3202883867</p>
-        <p class="conta1"></p>
-        <p class="conta">Contacto</p>
-        </section>
-
-        <section class="bloque2">
-        <p class="email">Vamendoza19@misena.edu.co</p>
-        <p class="conta2"></p>
-        <p class="corre">Email</p>
-        </section>
-        <br>
-        <p class="año">2021</p>
-    </footer>
-    
-    
+        <div class="social-item">
+          <a href="#"><img src="https://img.icons8.com/bubbles/100/000000/behance.png" /></a>
+        </div>
+      
+  </section>
+  <!-- End Footer -->
+  <script src="estilo.js"></script>
 </body>
+
 </html>
