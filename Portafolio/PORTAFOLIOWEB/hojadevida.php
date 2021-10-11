b198297c2eb80c8349298ccc0c51a071482f3b2f
vamendoza <vamendoza19@misena.edu.co>
2021-10-11 00:01:28 -0500

    HOJA DE VIDA

--- Portafolio/PORTAFOLIOWEB/hojadevida.php
+++ Portafolio/PORTAFOLIOWEB/hojadevida.php
@@ -4,12 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="CSS/index.css">
+    <link rel="stylesheet" href="CSS/estilo.css">
+    <link rel="stylesheet" href="CSS/hojadevida.css">
     <title>HOJA DE VIDA</title>
 </head>
 <body>
 
-    
-    
+    <!-- Menu -->
+    <section id="header">
+        <div class="header">
+            <div class="nav-bar">
+                <nav class="nav-list">
+                    <div class="hamburger">
+                        <div class="bar"></div>
+                    </div>
+                        <ul>
+                            <li><a href="index.php" data-after="Service"><strong>PORTAFOLIO</strong></a></li>
+                        </ul>
+                </nav>
+            </div>
+        </div>
+    </section>
+  <!-- FIN MENU -->
+
+
+ <div class="contenedor">
+
+ </div>
+
+
+ EN DESARROLLO ______
+
+
+
+
+<script src="estilo.js"></script>
 </body>
 </html>
